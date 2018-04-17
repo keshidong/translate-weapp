3c50b2832bce3401d224169e08aded0121291167
keshidong <keshidong.whu@gmail.com>
2018-04-17 23:13:34 +0800

    MOD: 添加了滑动切换select

--- components/slider-select/index.js
+++ components/slider-select/index.js
@@ -14,19 +14,51 @@ Component({
     },
     data: {
         value: 1,
+
+        // touch
+        _touchStartOffsetX: 0,
+        _touchThreshold: 50,
     },
     methods: {
         touchStart: function (e) {
-            console.log(e);
+            this.setData({
+                _touchStartOffsetX: e.changedTouches[0].clientX,
+            });
         },
         touchEnd: function (e) {
-            console.log(e);
+            const { _touchStartOffsetX, _touchThreshold } = this.data;
+            const _touchEndOffsetX = e.changedTouches[0].clientX;
+            const { options } = this.data;
+
+            if (_touchEndOffsetX - _touchStartOffsetX > _touchThreshold) {
+                // 右滑
+                const curIndex = this._getSelectActiveIndex();
+                if (curIndex - 1 >= 0) {
+                    // index 有效
+                    this.setData({
+                        value: options[curIndex - 1].value,
+                    });
+                }
+                
+            } else if (_touchEndOffsetX - _touchStartOffsetX < _touchThreshold) {
+                // 左滑
+                const curIndex = this._getSelectActiveIndex();
+                // index 有效
+                if (curIndex + 1 <= options.length) {
+                    this.setData({
+                        value: options[curIndex + 1].value,
+                    });
+                }
+            }
         },
         switchSelect: function (e) {
-            console.log(e);
             this.setData({
                 value: e.target.dataset.value,
             });
+        },
+        _getSelectActiveIndex: function () {
+            const { options, value } = this.data;
+            return options.map(opt => (opt.value)).indexOf(value);
         },
         _setStateFromProps: function () {
             this.setData({
