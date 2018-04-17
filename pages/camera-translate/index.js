b00fef06b7dc9638d4586adf5f2a0ee462c1a0ed
keshidong <keshidong.whu@gmail.com>
2018-04-18 01:06:31 +0800

    MOD: page events

--- pages/camera-translate/index.js
+++ pages/camera-translate/index.js
@@ -1,6 +1,20 @@
 Page({
     data: {},
     onLoad() {
-        this.ctx = wx.createCameraContext()
+        this.__cameraContext = wx.createCameraContext()
+    },
+    startRecord: function (e) {
+        this.__cameraContext.startRecord({
+            success: function (res) {
+                console.log(res, 'start');
+            }
+        })
+    },
+    stopRecord: function (e) {
+        this.__cameraContext.stopRecord({
+            success: function (res) {
+                console.log(res, 'end');
+            }
+        })
     }
 })
