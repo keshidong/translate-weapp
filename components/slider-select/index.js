cfe757ea8e421ff242dd718474fdadd93782b51d
keshidong <shidongke213666@sohu-inc.com>
2018-04-17 21:12:56 +0800

    MOD: slider-select 点击切换

--- components/slider-select/index.js
+++ components/slider-select/index.js
@@ -2,13 +2,36 @@ Component({
     options: {
         multipleSlots: true // 在组件定义时的选项中启用多slot支持
     },
-    properties: { /* ... */ },
+    properties: {
+        options: {
+            type: Array,
+            value: [],
+        },
+        style: {
+            type: String,
+        },
+        defaultValue: Number,
+    },
+    data: {
+        value: 1,
+    },
     methods: {
         touchStart: function (e) {
             console.log(e);
         },
         touchEnd: function (e) {
             console.log(e);
-        }
+        },
+        switchSelect: function (e) {
+            console.log(e);
+            this.setData({
+                value: e.target.dataset.value,
+            });
+        },
+        _setStateFromProps: function () {
+            this.setData({
+                value: this.defaultValue,
+            });
+        },
     }
 });
