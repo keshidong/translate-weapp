78e5b5519eb9295fbf7c3a862d17aec9a15510e6
keshidong <shidongke213666@sohu-inc.com>
2018-04-22 21:22:20 +0800

    MOD: mask

--- pages/camera-translate/index.js
+++ pages/camera-translate/index.js
@@ -1,7 +1,22 @@
 Page({
     data: {},
     onLoad() {
-        this.__cameraContext = wx.createCameraContext()
+        this.__cameraContext = wx.createCameraContext();
+    },
+    startCollectPhoto: function (e) {
+        this.__timeHandler = setInterval(() => {
+            this.__cameraContext.takePhoto({
+                success: function (res) {
+                    console.log(res, 'startCollectPhoto');
+                },
+                fail: function (res) {
+                    console.log(res, 'start collectPhoto failed');
+                }
+            })
+        }, 100);
+    },
+    stopCollectPhoto: function (e) {
+        clearInterval(this.__timeHandler);
     },
     startRecord: function (e) {
         this.__cameraContext.startRecord({
